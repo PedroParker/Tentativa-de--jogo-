92c7222377873b62baeb1d2add2e9c61f2594e10
TakingWW <jub_camar@hotmail.com>
2023-01-25 21:01:25 -0400

    Adding logic to the game

---
+++ Game_Functions.py
@@ -0,0 +1,34 @@
+# + respostas / qnt players * 0.8
+
+def welcome():
+    return "You are welcome to the divination game"
+
+
+def collect_ansewrs(players):
+    players_dictionary = {}
+    total = 0
+    for player in range(players):
+        player_name = input("Coloque seu nome: ")
+        player_ansewrs = int(input("Esconlha um número entre 1 e 100: "))
+        players_dictionary[player_name] = player_ansewrs
+        total += player_ansewrs
+        total = total / players * 0.8
+        ansewr_list = [players_dictionary, total]
+    return ansewr_list
+
+
+def winner(ansewr_list):
+    welcome()
+    total = ansewr_list[1]
+    error = 100
+    winner_player = ""
+    for player in ansewr_list[0]:
+        if abs(ansewr_list[0][player] - total) <= error:
+            error = abs(ansewr_list[0][player] - total)
+            winner_player = player
+            print(total)
+    print(winner_player)
+    return winner_player
+
+
+winner(collect_ansewrs(3))
