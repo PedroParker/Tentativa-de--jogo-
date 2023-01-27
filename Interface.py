29a848cd049327515e291f40c9ecd365649ee0df
TakingWW <jub_camara@hotmail.com>
2023-01-26 21:11:47 -0400

    Modifying the UI

--- Interface.py
+++ Interface.py
@@ -4,44 +4,50 @@ import pygame
 class Interface(pygame.sprite.Sprite):
     def __init__(self, settings):
         super().__init__()
-        # self.image = pygame.image.load("ImageDirectory.png")
-        self.buttom1_active = False
-        self.buttom1_color = settings.color_passive
-        self.buttom1_rect = pygame.Rect(200, 200, 140, 32)
-        self.buttom1_text = ""
+        self.image = pygame.image.load("Images/box.png")
+        self.rect = self.image.get_rect()
+        self.button1_active = False
+        self.button1_color = settings.color_passive
+        self.button1_rect = self.image.get_rect()
+        self.button1_rect.topleft = (settings.button_x, settings.button_y)
+        self.button1_text = ""
 
-        self.buttom2_active = False
-        self.buttom2_color = settings.color_passive
-        self.buttom2_rect = pygame.Rect(200, 500, 140, 32)
-        self.buttom2_text = ""
+        self.button2_active = False
+        self.button2_color = settings.color_passive
+        self.button2_rect = self.image.get_rect()
+        self.button2_rect.topleft = (
+            settings.button_x,
+            settings.button_y + settings.button_y_space,
+        )
+        self.button2_text = ""
 
-        self.mensagem_text_rect = pygame.Rect(200, 50, 140, 32)
-        self.mensagem_text = "Welcome"
-        self.mensagem_text_color = (0, 0, 0)
+        self.mensagem_text_rect = pygame.Rect(settings.width / 2, 40, 250, 32)
+        self.mensagem_text = "Welcome to the divination game"
+        self.mensagem_text_color = (255, 255, 255)
 
     def update_text(self, settings):
-        if self.buttom1_active:
-            self.buttom1_text = settings.user_text
-        elif self.buttom2_active:
-            self.buttom2_text = settings.user_text
+        if self.button1_active:
+            self.button1_text = settings.user_text
+        elif self.button2_active:
+            self.button2_text = settings.user_text
 
-    def blit(self, base_font, screen):
-        buttom1_text_surface = base_font.render(
-            self.buttom1_text, True, (255, 255, 255)
+    def blit(self, base_font, screen, settings):
+        button1_text_surface = base_font.render(
+            self.button1_text, True, settings.font_color
         )
         screen.blit(
-            buttom1_text_surface,
-            (self.buttom1_rect.x + 5, self.buttom1_rect.y + 5),
+            button1_text_surface,
+            (self.button1_rect.x + 5, self.button1_rect.y),
         )
-        buttom2_text_surface = base_font.render(
-            self.buttom2_text, True, (255, 255, 255)
+        button2_text_surface = base_font.render(
+            self.button2_text, True, settings.font_color
         )
         screen.blit(
-            buttom2_text_surface,
-            (self.buttom2_rect.x + 5, self.buttom2_rect.y + 5),
+            button2_text_surface,
+            (self.button2_rect.x + 5, self.button2_rect.y),
         )
         mensagem_text_surface = base_font.render(
-            self.mensagem_text, True, (255, 255, 255)
+            self.mensagem_text, True, settings.font_color
         )
         screen.blit(
             mensagem_text_surface,
--- Settings.py
+++ Settings.py
@@ -5,8 +5,15 @@ class Settings:
     def __init__(self):
         self.width = 640
         self.height = 600
+        self.backgroud_color = (255, 255, 255)
         self.color_active = pygame.Color("lightskyblue3")
         self.color_passive = pygame.Color("chartreuse4")
+        self.button_x = 200
+        self.button_y = 200
+        self.button_y_space = 50
+        self.button_width = 200
+        self.button_height = 40
         self.clock = pygame.time.Clock()
+        self.font_color = (0, 0, 0)
+        self.font = "/usr/share/fonts/truetype/CascadiaCode/ttf/CascadiaCodeItalic.ttf"
         self.user_text = ""
-        self.count = 0
