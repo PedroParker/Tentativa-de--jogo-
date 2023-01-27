c564de86c97b1ff2e569ca93343b3b0922ff6f47
TakingWW <jub_camara@hotmail.com>
2023-01-27 15:55:23 -0400

    Adding totating cards to background

--- Game_Functions.py
+++ Game_Functions.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from Card import Card
 
 
 def events(settings, interface):
@@ -7,48 +8,59 @@ def events(settings, interface):
     for event in pygame.event.get():
         pygame.key.set_repeat(300, 100)
         if event.type == pygame.QUIT:
-            print(interface.button2_text)
             pygame.quit()
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print(event.pos)
-            print(interface.button1_rect.topleft)
             if interface.button1_rect.collidepoint(event.pos):
-                print("colidiu 1")
+                interface.button1_empty = False
                 interface.button1_active = True
                 settings.user_text = interface.button1_text
             else:
                 interface.button1_active = False
+                if interface.button1_text == "":
+                    interface.button1_empty = True
 
             if interface.button2_rect.collidepoint(event.pos):
-                print("colidiu 2")
+                interface.button2_empty = False
                 interface.button2_active = True
                 settings.user_text = interface.button2_text
             else:
                 interface.button2_active = False
+                if interface.button2_text == "":
+                    interface.button2_empty = True
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                pygame.quit()
+                sys.exit()
         if event.type == pygame.KEYDOWN and (
             interface.button1_active or interface.button2_active
         ):
             if event.key == pygame.K_BACKSPACE:
                 settings.user_text = settings.user_text[:-1]
             elif (
-                len(settings.user_text) <= 10
+                len(settings.user_text) <= 9
                 and event.key != pygame.K_ESCAPE
                 and event.key != pygame.K_RETURN
             ):
                 settings.user_text += event.unicode
 
 
-def drawing(screen, interface, settings):
-    screen.blit(interface.image, (settings.button_x, settings.button_y))
-    screen.blit(
-        interface.image,
-        (settings.button_x, settings.button_y + settings.button_y_space),
-    )
+def drawing(screen, interface, settings, card_group):
     pygame.draw.rect(
         screen, interface.mensagem_text_color, interface.mensagem_text_rect
+    )
+    settings.count += 1
+    if settings.count % 100 == 0:
+        card = Card(settings)
+        card_group.add(card)
+        settings.count = 0
+    card_group.update(screen, settings)
+    screen.blit(interface.box_image, (settings.button_x, settings.button_y))
+    screen.blit(
+        interface.box_image,
+        (settings.button_x, settings.button_y + settings.button_y_space),
     )
 
 
--- Run.py
+++ Run.py
@@ -3,16 +3,17 @@ from Settings import Settings
 import Game_Functions as game_f
 from Interface import Interface
 
-settings = Settings()
-interface = Interface(settings)
+card_group = pygame.sprite.Group()
 pygame.init()
-screen = pygame.display.set_mode((settings.width, settings.height))
+screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+settings = Settings(screen)
+interface = Interface(settings)
 base_font = pygame.font.Font(settings.font, 32)
 pygame.display.set_caption("Divination Game")
 while True:
     game_f.events(settings, interface)
     screen.fill(settings.backgroud_color)
-    game_f.drawing(screen, interface, settings)
+    game_f.drawing(screen, interface, settings, card_group)
     game_f.blit(base_font, settings, screen, interface)
     pygame.display.flip()
     settings.clock.tick(60)
